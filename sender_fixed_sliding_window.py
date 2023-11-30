ed1ac6e4e163f5c225120cc0d861ecc38db644c6
krystalchau <krystalchau.20@gmail.com>
2023-11-29 23:09:00 -0800

    Added starter code and wrote to do comments
    Co-authored-by: Jacob Feenstra <Hawkhobo@users.noreply.github.com>

---
+++ sender_fixed_sliding_window.py
@@ -0,0 +1,4 @@
+
+# TO DO: 
+# Send window size of packets, wait for ACKs, slide window if first packet's ACK is recieved
+# if ACK is not recieved by timeout, send again double timeout but do not slide window
