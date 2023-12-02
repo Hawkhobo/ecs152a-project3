5c3d52ba959fd2a8980166eaf35160dca4f3e8d2
Hawkhobo <jacob.d.feenstra@gmail.com>
2023-12-01 22:23:33 -0800

    stop-and-wait mkI
    Co-authored-by: krystalchau <krystalchau@users.noreply.github.com>

--- sender_stop_and_wait.py
+++ sender_stop_and_wait.py
@@ -7,7 +7,7 @@
 # Sending `file.mp3`, 1024 bytes per packet 
 
 import socket
-from datetime import datetime
+from time import time 
 
 # total packet size
 PACKET_SIZE = 1024
@@ -15,63 +15,75 @@ PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
 # bytes available for message
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
-# total packets to send
-WINDOW_SIZE = 20
 
 # read data
-with open('send.txt', 'rb') as f:
+with open('./docker/file.mp3', 'rb') as f:
     data = f.read()
  
+total_packet_delay = 0
+packetCount = 0
+
 # create a udp socket
+start_throughput = time()
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
 
     # bind the socket to a OS port
     udp_socket.bind(("0.0.0.0", 5000))
-    udp_socket.settimeout(1)
+
+    timeoutDuration = 1
     
     # start sending data from 0th sequence
     seq_id = 0
+    # Run a timer for throughput
     while seq_id < len(data):
         
-        # create messages
-        messages = []
-        acks = {}
-        seq_id_tmp = seq_id
-        for i in range(WINDOW_SIZE):
-            # construct messages
-            # sequence id of length SEQ_ID_SIZE + message of remaining PACKET_SIZE - SEQ_ID_SIZE bytes
-            message = int.to_bytes(seq_id_tmp, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id_tmp : seq_id_tmp + MESSAGE_SIZE]
-            messages.append((seq_id_tmp, message))
-            acks[seq_id_tmp] = False
-            # move seq_id tmp pointer ahead
-            seq_id_tmp += MESSAGE_SIZE
+        udp_socket.settimeout(1)
+        
+        # construct message
+        # sequence id of length SEQ_ID_SIZE + message of remaining PACKET_SIZE - SEQ_ID_SIZE bytes
+        message = int.to_bytes(seq_id, SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id : seq_id + MESSAGE_SIZE]
 
-        # send messages
-        for _, message in messages:
-            udp_socket.sendto(message, ('localhost', 5001))
+        # send message
+        udp_socket.sendto(message, ('localhost', 5001))
+        packet_delay = time()
+        packetCount += 1
         
         # wait for acknowledgement
         while True:
             try:
                 # wait for ack
                 ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+                total_packet_delay += time() - packet_delay
                 
                 # extract ack id
                 ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
-                print(ack_id, ack[SEQ_ID_SIZE:])
-                acks[ack_id] = True
+                break
                 
-                # all acks received, move on
-                if all(acks.values()):
-                    break
             except socket.timeout:
-                # no ack received, resend unacked messages
-                for sid, message in messages:
-                    if not acks[sid]:
-                        udp_socket.sendto(message, ('localhost', 5001))
+                # no ack received, resend unacked message
+                timeoutDuration += timeoutDuration
+                udp_socket.settimeout(timeoutDuration)
+                udp_socket.sendto(message, ('localhost', 5001))
                 
         # move sequence id forward
-        seq_id += MESSAGE_SIZE * WINDOW_SIZE
-        
+        seq_id += MESSAGE_SIZE
+    
+    # Run the time until the last packet from the file, and NOT the final closing message. 
+    end_throughput = time()
+    
+    # get throughput
+    throughput  = len(data) / (end_throughput - start_throughput)
+
+    # get average packet delay
+    avg_packet_delay = total_packet_delay / packetCount
+
+    # get performance metric (throughput/average per packet delay)
+    performance_metric = throughput / avg_packet_delay
+
+    print(f'{round(throughput, 2)}, {round(avg_packet_delay, 2)}, {round(performance_metric, 2)}')
+    
     # send final closing message
     udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
+    
+    # close the connection
+    udp_socket.close()
